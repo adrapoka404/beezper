1fae296890ccaddce185ce6769da4ca427e79dba
adrapoka404 <adrapok@gmail.com>
2022-03-30 14:37:07 -0600

    solo ladrillos

--- resources/views/components/no-solo-ladrillos.blade.php
+++ resources/views/components/no-solo-ladrillos.blade.php
@@ -1,23 +1,23 @@
 <div class=" bg-fondo-home bg-no-repeat bg-cover h-auto items-center text-center">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8  ">
         <div class=" h-5"></div>
-        <div class="text-purple-200 text-4xl font-quick font-bold mt-96" id="somos">No somos sólo ladrillos,</div>
-        <div class="text-purple-200 text-xl font-quick font-semibold mt-10">con nosotros puedes diversificar tus inversiones
+        <div class="text-blue-300 text-4xl font-quick font-bold mt-96" id="somos">No somos sólo ladrillos,</div>
+        <div class="text-blue-300 text-xl font-quick font-semibold mt-10">con nosotros puedes diversificar tus inversiones
         </div>
         <div class="md:flex md:justify-center my-28 ">
-            <div class=" h-52 w-72 bg-white rounded-3xl my-10 md:my-auto grid mx-auto">
-                <div class="text-blue-300 font-extralight text-2xl w-32 mx-auto my-auto right-0 h-20">
+            <div class=" h-52 w-72 bg-white rounded-3xl my-10 md:my-auto grid mx-auto opacity-60 ">
+                <div class="text-blue-300 font-extrabold text-3xl w-32 mx-auto my-auto right-0 h-20 ">
                     Inmobiliario
                 </div>
             </div>
-            <div class=" h-52 w-72 bg-white rounded-3xl my-10 md:my-autogrid mx-auto md:mx-5 shadow-sm shadow-blue-400">
-                <div class="text-blue-300 font-extralight text-2xl w-32 mx-auto my-auto right-0 h-20">
+            <div class=" h-52 w-72 bg-white rounded-3xl my-10 md:my-autogrid mx-auto md:mx-5 opacity-60 shadow-sm shadow-blue-300 ">
+                <div class="text-blue-300 font-extrabold text-3xl w-32 mx-auto my-auto right-0 h-20 ">
                     Productos<br>y <br>servicios
                 </div>
             </div>
 
-            <div class=" h-52 w-72 bg-white rounded-3xl my-10 md:my-auto grid mx-auto">
-                <div class="text-blue-300 font-extralight text-2xl w-32 mx-auto my-auto right-0 h-20">
+            <div class=" h-52 w-72 bg-white rounded-3xl my-10 md:my-auto grid mx-auto opacity-60">
+                <div class="text-blue-300  font-extrabold text-3xl w-32 mx-auto my-auto right-0 h-20 ">
                     Industria
                 </div>
             </div>
