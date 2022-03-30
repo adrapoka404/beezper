972f68faae852a9dcdb940255c684184c5713d8e
adrapoka404 <adrapok@gmail.com>
2022-03-30 14:52:40 -0600

    propuesta ladrillos sombras

--- resources/views/components/no-solo-ladrillos.blade.php
+++ resources/views/components/no-solo-ladrillos.blade.php
@@ -5,18 +5,18 @@
         <div class="text-blue-300 text-xl font-quick font-semibold mt-10">con nosotros puedes diversificar tus inversiones
         </div>
         <div class="md:flex md:justify-center my-28 ">
-            <div class=" h-52 w-72 bg-white rounded-3xl my-10 md:my-auto grid mx-auto opacity-60 ">
+            <div class=" h-52 w-72 bg-white rounded-3xl my-10 md:my-auto grid mx-auto shadow-2xl shadow-black ">
                 <div class="text-blue-300 font-extrabold text-3xl w-32 mx-auto my-auto right-0 h-20 ">
                     Inmobiliario
                 </div>
             </div>
-            <div class=" h-52 w-72 bg-white rounded-3xl my-10 md:my-autogrid mx-auto md:mx-5 opacity-60 shadow-sm shadow-blue-300 ">
+            <div class=" h-52 w-72 bg-white rounded-3xl my-10 md:my-autogrid mx-auto md:mx-5 shadow-2xl shadow-blue-200">
                 <div class="text-blue-300 font-extrabold text-3xl w-32 mx-auto my-auto right-0 h-20 ">
                     Productos<br>y <br>servicios
                 </div>
             </div>
 
-            <div class=" h-52 w-72 bg-white rounded-3xl my-10 md:my-auto grid mx-auto opacity-60">
+            <div class=" h-52 w-72 bg-white rounded-3xl my-10 md:my-auto grid mx-auto shadow-2xl shadow-purple-200">
                 <div class="text-blue-300  font-extrabold text-3xl w-32 mx-auto my-auto right-0 h-20 ">
                     Industria
                 </div>
